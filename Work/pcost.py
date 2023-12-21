4351480a343ad88d299f508e1dc4f22d4e20e7e4
bbejeck <bbejeck@gmail.com>
2023-12-21 17:40:27 -0500

    Add portfolio cost calculation in pcost.py
    Implemented a function called 'portfolio_cost' in 'pcost.py' that calculates the total cost of portfolio based on provided data. It takes a 'filename' as an argument, reads the respective file and computes the total_cost by taking shares and cost into consideration. It also takes care of incorrect values by using try and exception.
    
    Completion of the Introduction section of the Python course

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,31 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import csv
+import sys
+
+
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename) as pf:
+        rows = csv.reader(pf)
+        next(rows)
+        for row in rows:
+            try:
+                shares = int(row[1])
+                cost = float(row[2].strip())
+                total_cost = total_cost + (shares * cost)
+            except ValueError:
+                print(f'Problem parsing a value in the row {row}')
+
+    return total_cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+the_cost = portfolio_cost(filename)
+print(f'Total cost {the_cost}')

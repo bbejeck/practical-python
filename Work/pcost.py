40eb9a8e4bef370d4aa914a7cedd71a393bc3ab7
bbejeck <bbejeck@gmail.com>
2023-12-22 20:29:12 -0500

    Implement portfolio reporting in report.py and enhance pcost.py
    Added functionality in report.py to read portfolio and price data, generate a report, and display it. Adjusted the error handling in pcost.py to provide more detailed information about which row caused an issue during reading the file. The modification in pcost.py improved adherence to the DRY principle by using a dictionary for data record.

--- Work/pcost.py
+++ Work/pcost.py
@@ -10,14 +10,15 @@ def portfolio_cost(filename):
     total_cost = 0.0
     with open(filename) as pf:
         rows = csv.reader(pf)
-        next(rows)
-        for row in rows:
+        headers = next(rows)
+        for n, row in enumerate(rows, start=1):
             try:
-                shares = int(row[1])
-                cost = float(row[2].strip())
+                d = dict(zip(headers, row))
+                shares = int(d['shares'])
+                cost = float(d['price'].strip())
                 total_cost = total_cost + (shares * cost)
             except ValueError:
-                print(f'Problem parsing a value in the row {row}')
+                print(f'Row {n}: Couldn\'t parse row {row}')
 
     return total_cost
 
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,60 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+from pprint import pprint
+from math import floor
+
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename) as pf:
+        rows = csv.reader(pf)
+        headers = next(rows)
+        for row in rows:
+            d = dict(zip(headers, row))
+            try:
+                portfolio.append({"name": d['name'], "shares": int(d['shares']), "price": float(d['price'])})
+            except ValueError:
+                print(f'Problem parsing a value in the row {row}')
+
+    return portfolio
+
+
+def get_prices(filename):
+    price_map = {}
+    with open(filename) as price_file:
+        rows = csv.reader(price_file)
+        for row in rows:
+            if row:
+                price_map[row[0]] = row[1]
+
+    return price_map
+
+
+def print_row(row_info):
+    print(f'{row_info[0]:>12} {row_info[1]:>12} {row_info[2]:>12} {row_info[3]:>12.2f}')
+
+
+def make_report(portfolio, prices):
+    dash = '-'
+    portfolio_changes = []
+    headers = ("Name", "Shares", "Price", "Change")
+    for position in portfolio:
+        if position['name'] in prices:
+            delta = float(prices[position['name']]) - position['price']
+            portfolio_changes.append((position['name'], position['shares'], prices[position['name']], delta))
+
+    print(f'{headers[0]:>12} {headers[1]:>12} {headers[2]:>12} {headers[3]:>12}')
+    print(f'{dash:-<12}  {dash:-<12} {dash:-<12} {dash:-<12}')
+    for portfolio_change in portfolio_changes:
+        print_row(portfolio_change)
+
+
+portfolio = read_portfolio('Data/portfoliodate.csv')
+prices = get_prices('Data/prices.csv')
+
+# pprint(portfolio)
+# pprint(prices)
+
+make_report(portfolio, prices)
